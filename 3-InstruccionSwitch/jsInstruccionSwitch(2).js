ccc56bfa3325e33fcc86bc283f3ffb01a6944cfd
JessicaFontenez <60699459+JessicaFontenez@users.noreply.github.com>
2020-02-18 10:56:25 -0300

    para completar

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -5,6 +5,43 @@ var mesDelAño = document.getElementById('mes').value;
 
 alert (mesDelAño);
 
+	switch(mesDelAño)
+	{
+		case 1:
+			switch(mesDelAño)
+			{
+			case "Julio":
+			case "Agosto":
+				alert("Abrigate que hace frio.");
+				break;
+			}
+
+		case 2:
+			switch(mesDelAño)
+			{
+			case "Enero":
+			case"Febrero":
+			case"Marzo":
+			case "Abril":
+			case "Mayo":
+			case "Junio":
+				alert("Falta para el invierno.");
+				break;
+			}
+
+		case 3:
+			switch(mesDelAño)
+			{
+			case "Septiembre":
+			case "Octubre":
+			case "Noviembre":
+			case "Diciembre":
+				alert("Ya pasamos el frio, ahora calor!!!.");
+				break;
+			}
+
+	}
+
 
 
 
