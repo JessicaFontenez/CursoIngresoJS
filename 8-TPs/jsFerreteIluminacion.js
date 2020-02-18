715f6ab3f1749f69a7187abe4f76b1f0a71f367e
JessicaFontenez <60699459+JessicaFontenez@users.noreply.github.com>
2020-02-18 10:57:34 -0300

    revisar

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -10,5 +10,118 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
+	var cantidadDeLamparas;
+	var marca;
+	var precioBruto;
+	var descuento;
+	var precioConDescuento;
+
+	cantidadDeLamparas=document.getElementById('Cantidad').value;
+	cantidadDeLamparas=parseInt(cantidadDeLamparas);
+	marca=document.getElementById('Marca').value;
+	precioBruto=cantidadDeLamparas*35;
+	console.info("Bruto",precioBruto);
+	descuento= 0;
+	switch(cantidadDeLamparas)
+	{
+		case 5:
+
+			switch(marca)
+			{
+				case "ArgentinaLuz";
+					descuento=40;
+				default:
+					descuento=30;
+					breake;
+			case 4:
+				switch(marca)
+				{
+					case "ArgentinaLuz";
+					case "FelipeLamparas";
+						descuento=25;
+						breake;
+					default:
+						descuento=20;
+						breake;
+				case 3:
+					switch(marca)
+					{
+						case "ArgentinaLuz";
+						case "FelipeLamparas";
+							descuento=10;
+							breake;
+						default:
+							descuento=5;
+							breake;
+
+							case 1:
+							case 2:
+								descuento=0
+								breake;
+								descuento=50
+								breake;
+
+	}
+
+	if (cantidadDeLamparas>5)
+	 {
+	 	descuento=50;
+	 }
+	 else	
+	 {
+	 	if (cantidadDeLamparas==5) 
+	 	{
+	 		if (marca=="ArgentinaLuz") 
+	 		{
+	 			descuento=40;
+	 		} else
+	 		{
+	 			descuento=30;
+	 		}
+	 	}
+	 	else
+	 	{
+	 		if (cantidadDeLamparas==4) 
+	 			{
+	 			
+		 		if (marca=="ArgentinaLuz"||marca=="FelipeLamparas") 
+		 		{
+		 			descuento=25;
+		 		} 
+		 		else
+		 		{
+		 			descuento=20;
+	 			}
+	 		else
+	 		{
+	 			if (cantidadDeLamparas==3) 
+	 			{
+					if (marca=="ArgentinaLuz") 
+				 	{
+				 		descuento=40;
+				 	}
+				 	 else	
+				 	{
+					 		
+						if (marca=="FelipeLamparas"||marca=="ArgentinaLuz") 
+						{
+						descuento=10;
+						} 
+						else
+						{
+						descuento=5;
+						}	
+	 			 		
+	 			 	 }
+	 			 }
+	 		}
+	 	}
+
+	 }
+
+
+	precioConDescuento= precioBruto - precioBruto * descuento/100;
+	console.info("Descuento",precioConDescuento);
+	document.getElementById('precioDescuento').value=precioConDescuento
  	
 }
