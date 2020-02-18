2595df9f0dd8d3c5d7090fb46753ec190e383813
JessicaFontenez <60699459+JessicaFontenez@users.noreply.github.com>
2020-02-18 20:42:39 -0300

    Completos

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,6 +1,17 @@
 function mostrar()
 {
 //tomo la edad  
+var edad
+
+edad=document.getElementById("edad").value;
+
+
+if (edad == 15) {
+	alert("niña bonita")
+	} else 
+		{ 
+			alert (edad)
+		}
 
 
 
