ee7c1cb7e29a9d034ec994649f672c85c1eca297
JessicaFontenez <60699459+JessicaFontenez@users.noreply.github.com>
2020-02-26 01:54:42 -0300

    Completos

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -5,6 +5,16 @@ function mostrar()
 	var acumulador=0;
 	var respuesta='si';
 
+	while(respuesta=="si")
+		{	
+			numero=prompt("Ingrese un numero");
+			numero=parseInt(numero);
+			acumulador=acumulador+numero;
+			acumulador=parseInt(acumulador);
+			contador++;
+			respuesta=prompt("¿Desea seguir ingresando numeros?");
+		}
+
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
