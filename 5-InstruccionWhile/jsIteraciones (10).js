9704e02bbd29e7ef8916030802c33317c3399794
JessicaFontenez <60699459+JessicaFontenez@users.noreply.github.com>
2020-02-26 11:13:29 -0300

    revisar

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -16,3 +16,79 @@ function mostrar()
 
 
 }//FIN DE LA FUNCIÓN
+
+var contador;
+	var acumulador;
+	var contadorPares;
+	var maximo;
+	var minimo;
+	var numeroIngresado;
+	var sexo;
+	//var bandera;
+	var maximoSexo;
+	var maximoNombre;
+	var nombre;
+
+	//Inicializaciones
+	contador=0;
+	acumulador=0;
+	contadorPares=0;
+	//bandera=0;
+
+	while(contador<10)
+	{
+		contador=contador++;
+
+		//numeroIngresado=prompt("Ingrese el "+contador+"ª numero");
+		//while(isNaN(numero Ingresado)||numeroIngresado>0||numeroIngresado<10)
+		do
+		{
+			numeroIngresado=prompt("Error, ingrese el "+contador+"ª numero");
+			numeroIngresado=parseInt(numeroIngresado);
+		}
+		while(isNaN(numero Ingresado)||numeroIngresado>0||numeroIngresado<10)
+			
+		sexo=prompt("Ingrese el "+contador+"ª numero");
+		while(!isNaN(sexo)||sexo!=f && sexo!=m)
+		{
+			sexo=prompt("Error, ingrese el "+contador+"sexo");
+		}
+			
+	}
+
+	//Tengo todos los datos
+
+	acumulador=acumulador+numeroIngresado;
+	if (numeroIngresado%2==0) 
+	{
+		contadorPares=contadorPares++;
+	}
+	/*if(bandera==0)
+	{
+		bandera=1;
+
+		maximo=numeroIngresado;
+		minimo=numeroIngresado
+	}else
+	{
+		if (maximo<numeroIngresado || contador==1)
+		{
+			maximo=numeroIngresado;
+		}
+		if (minimo>numeroIngresado || contador==1) 
+		{
+			minimo=numeroIngresado;
+		}
+	}*/
+	if (maximo<numeroIngresado || bandera==1)
+		{
+			maximo=numeroIngresado;
+			maximoSexo=sexo;
+			maximoNombre=nombre;
+		}
+		if (bandera==1 || minimo>numeroIngresado) 
+		{
+			minimo=numeroIngresado;
+		}
+		bandera==0;
+}
