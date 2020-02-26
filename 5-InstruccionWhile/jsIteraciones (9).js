d528ebb1276f3144c736c7c50030cefbf213e3c6
JessicaFontenez <60699459+JessicaFontenez@users.noreply.github.com>
2020-02-26 01:55:29 -0300

    Revisar

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -3,14 +3,38 @@ function mostrar()
 
 	var contador=0;
 	// declarar variables
+	var numero;
+	var acumulador;
+	var numeroMayor;
+	var numeroMenor;
+	numeroMenor=0;
 	
 	var respuesta='si';
 
-	while(respuesta!='no')
+	while(respuesta!='no'&& respuesta==isNaN || respuesta=="si")
 	{
-		
+		numero=prompt("Ingrese un numero");
+		numero=parseInt(numero);
+		if (numero>0) {
+						numeroMayor=numero;
+					}
+					else{
+						numeroMenor=numero;
+					}
+					while(numero>numeroMayor){
+												numeroMayor=numero;
+											}	
+											while(numero<numeroMenor){
+												numeroMenor=numero;
+											}
+
+		contador++;
+		respuesta=prompt("¿Desea seguir ingresando numeros?")
 	
 	}
+
+	document.getElementById('minimo').value=numeroMenor;
+	document.getElementById('maximo').value=numeroMayor;
 
 
 
